ed3a6d1d6a13d5afac9354990568f29feb1b3ff4
秋及北 <wanglinlee1707@gmail.com>
2024-09-25 18:50:44 +0800

    add component

---
+++ lib/component/bottom_bar.dart
@@ -0,0 +1,73 @@
+import 'package:drill_app/constant/router.dart';
+import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+
+enum BottomBarIndex {
+  home,
+  business,
+  personal,
+}
+
+class BottomBar extends StatefulWidget {
+  const BottomBar(
+      {super.key,
+      required this.title,
+      required this.selectedIndex,
+      required this.body});
+
+  final String title;
+  final BottomBarIndex selectedIndex;
+  final Widget body;
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  final log = Logger('BottomBar');
+
+  void _onItemTapped(int index) {
+    final selectedTab = BottomBarIndex.values[index];
+    switch (selectedTab) {
+      case BottomBarIndex.home:
+        Navigator.pushReplacementNamed(context, home);
+        break;
+      case BottomBarIndex.business:
+        Navigator.pushReplacementNamed(context, home);
+        break;
+      case BottomBarIndex.personal:
+        Navigator.pushReplacementNamed(context, personal);
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: widget.body,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Personal',
+          ),
+        ],
+        currentIndex: widget.selectedIndex.index,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
