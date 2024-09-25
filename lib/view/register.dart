78a61a4c19f7d736f0b4ed6c17360b3d89063cac
秋及北 <wanglinlee1707@gmail.com>
2024-09-25 17:18:09 +0800

    wip Register

---
+++ lib/view/register.dart
@@ -0,0 +1,100 @@
+import 'package:drill_app/api/api.dart';
+import 'package:drill_app/model/user.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:logging/logging.dart';
+import 'package:drill_app/constant/constant.dart' as constant;
+
+class Register extends StatefulWidget {
+  const Register({super.key});
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final log = Logger('Register');
+  final FlutterSecureStorage _secureStorage = const FlutterSecureStorage();
+
+  final CreateUserReq _createUserReq = CreateUserReq();
+
+  void _updateLoginReq(String input, CreateUserReqField field) {
+    switch (field) {
+      case CreateUserReqField.username:
+        _createUserReq.username = input;
+      case CreateUserReqField.password:
+        _createUserReq.password = input;
+    }
+  }
+
+  void _createUserFunc() async {
+    CreateUserResp? createUserResp = await api.userApi.createUser(_createUserReq).catchError((err) {
+      log.severe('api.userApi.createUser: $err');
+    });
+    if (createUserResp?.base?.code == 0) {
+      if (createUserResp?.data?.token != null) {
+        await _secureStorage.write(key: constant.token, value: createUserResp?.data?.token);
+        if (mounted) {
+          Navigator.pushReplacementNamed(context, '/landing');
+        }
+      }
+    }
+  }
+
+  Widget loginForm() {
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          width: 250,
+          child: TextField(
+            obscureText: true,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Username',
+            ),
+            controller: TextEditingController(text: _createUserReq.username),
+            onChanged: (value) =>
+                _updateLoginReq(value, CreateUserReqField.username),
+          ),
+        ),
+        SizedBox(
+          width: 250,
+          child: TextField(
+            obscureText: true,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Password',
+            ),
+            controller: TextEditingController(text: _createUserReq.password),
+            onChanged: (value) =>
+                _updateLoginReq(value, CreateUserReqField.password),
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Register"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            loginForm(),
+            TextButton(
+              onPressed: () {
+                _createUserFunc();
+              },
+              child: const Text("Register"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
