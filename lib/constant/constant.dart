c8ea61b4f2c3a1a5d8e9d94d8a025e34aae91f16
秋及北 <wanglinlee1707@gmail.com>
2024-09-25 10:34:33 +0800

    add constant

---
+++ lib/constant/constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const String token = "token";
+const String devTokens = "devTokens";
+
+final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
+    GlobalKey<ScaffoldMessengerState>();
---
+++ lib/constant/router.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+const String landing = "/";
+const String register = "/register";
+const String login = "/login";
+const String home = "/home";
+const String personal = "/personal";
+const String myDrill = "/my_drill";
+const String tokenManager = "/token_manager";
+const String createGroup = "/group/create";
+const String group = "/group";
+const String createEvent = "/createEvent";
+const String event = "/event";
