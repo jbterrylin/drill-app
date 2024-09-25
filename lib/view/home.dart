9f734a4a27cc581b7677030d89c74a5834f58169
秋及北 <wanglinlee1707@gmail.com>
2024-09-25 17:17:38 +0800

    wip Home

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
-  const Home({super.key, required this.title});
-
-  final String title;
+  const Home({super.key});
 
   @override
   State<Home> createState() => _HomeState();
@@ -36,7 +34,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text("Home"),
       ),
       body: Center(
         child: Column(
