55e0414d8a62c09d829e9695788584a337e291a2
秋及北 <wanglinlee1707@gmail.com>
2024-09-26 08:47:20 +0800

    update api

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,8 +1,10 @@
+import 'package:drill_app/api/group.dart';
 import 'package:drill_app/api/user.dart';
 import 'package:drill_app/util/http/http.dart';
 
 class Api {
   late UserApi userApi;
+  late GroupApi groupApi;
 
   Api() {
     init();
@@ -10,6 +12,7 @@ class Api {
 
   void init() async {
     userApi = UserApi(httpService: HttpService(baseUrl: 'http://10.0.2.2:3000'));
+    groupApi = GroupApi(httpService: HttpService(baseUrl: 'http://10.0.2.2:3001'));
   }
 }
 
--- lib/api/group.dart
+++ lib/api/group.dart
@@ -3,39 +3,41 @@ import '../util/http/http.dart';
 
 class GroupApi {
   final HttpService httpService;
-  final String baseUrl;
 
-  GroupApi({required this.httpService, required this.baseUrl});
+  GroupApi({required this.httpService});
   Future<OnlyId?> createGroup(CreateGroupReq input) async {
-    final data =
-        await httpService.post('$baseUrl/v1/group/create', input.toJson());
+    final data = await httpService.post('/v1/group/create', input.toJson());
     return OnlyId.fromJson(data);
   }
 
+  Future<GetGroupListResp?> getGroupList(GetGroupListReq input) async {
+    final data = await httpService.post('/v1/group/list', input.toJson());
+    return GetGroupListResp.fromJson(data);
+  }
+
   Future<OnlyId?> createGroupInvite(CreateGroupInviteReq input) async {
-    final data = await httpService.post(
-        '$baseUrl/v1/group_invite/create', input.toJson());
+    final data =
+        await httpService.post('/v1/group_invite/create', input.toJson());
     return OnlyId.fromJson(data);
   }
 
   Future<GetGroupInviteListResp?> getGroupInviteList(
       GetGroupInviteListReq input) async {
     final data =
-        await httpService.post('$baseUrl/v1/group_invite/list', input.toJson());
+        await httpService.post('/v1/group_invite/list', input.toJson());
     return GetGroupInviteListResp.fromJson(data);
   }
 
   Future<Base?> updateGroupInviteStatus(
       UpdateGroupInviteStatusReq input) async {
     final data = await httpService.post(
-        '$baseUrl/v1/group_invite/update/status', input.toJson());
+        '/v1/group_invite/update/status', input.toJson());
     return Base.fromJson(data);
   }
 
   Future<GetGroupUserListResp?> getGroupUserList(
       GetGroupUserListReq input) async {
-    final data =
-        await httpService.post('$baseUrl/v1/group_user/list', input.toJson());
+    final data = await httpService.post('/v1/group_user/list', input.toJson());
     return GetGroupUserListResp.fromJson(data);
   }
 }
