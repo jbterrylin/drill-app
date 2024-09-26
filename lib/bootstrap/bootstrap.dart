fb14251f23fc9bf77a02532a24ad21ad05c10ad5
秋及北 <wanglinlee1707@gmail.com>
2024-09-26 08:47:01 +0800

    update bootstrap

--- lib/bootstrap/bootstrap.dart
+++ lib/bootstrap/bootstrap.dart
@@ -1,8 +1,22 @@
+import 'package:logging/logging.dart';
+
 class Bootstrap {
+  final log = Logger('HttpService');
+
   Bootstrap() {
     init();
   }
 
   void init() async {
+    log.finest("init start");
+    initLogging();
+    log.finest("init end");
+  }
+
+  void initLogging() {
+    Logger.root.level = Level.ALL;
+    Logger.root.onRecord.listen((record) {
+      print('${record.loggerName} ${record.level.name}: ${record.time}: ${record.message}');
+    });
   }
 }
