f897660fab6121205e44939ae9de7b10a9f3c64e
秋及北 <wanglinlee1707@gmail.com>
2024-09-30 20:53:58 +0800

    wip Landing

--- lib/view/landing.dart
+++ lib/view/landing.dart
@@ -1,13 +1,11 @@
-import 'dart:convert';
-
-import 'package:drill_app/api/api.dart';
 import 'package:drill_app/constant/constant.dart' as constant;
 import 'package:drill_app/constant/router.dart';
-import 'package:drill_app/model/user.dart';
+import 'package:drill_app/state/me.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:get_it/get_it.dart';
 
-class Landing extends StatefulWidget { 
+class Landing extends StatefulWidget {
   const Landing({super.key});
 
   @override
@@ -20,29 +18,20 @@ class _LandingState extends State<Landing> {
   @override
   void initState() {
     super.initState();
-    init();  // 在 initState 中调用异步初始化方法
-  }
-
-  Future<UserResp?> getMeFunc() async {
-     UserResp? userResp = await api.userApi.getMe();
-    if (userResp?.base?.code == 0) {
-      await _secureStorage.write(key: constant.userInfo, value: jsonEncode(userResp?.toJson()));
-      await _secureStorage.write(key: constant.userInfoReceiveTime, value: DateTime.now().toIso8601String());
-    }
-    return userResp;
+    init(); // 在 initState 中调用异步初始化方法
   }
 
   Future<void> init() async {
     String? token = await _secureStorage.read(key: constant.token);
-        
+
     if (token == null) {
       // 如果没有 token，跳转到登录页面
       if (!mounted) return;
       Navigator.pushReplacementNamed(context, login);
     } else {
-     var userResp = await getMeFunc();
+      var userResp = await GetIt.I<MeController>().getMeWithRefresh();
       if (!mounted) return;
-      if (userResp?.base?.code == 0) {
+      if (userResp != null) {
         Navigator.pushReplacementNamed(context, home);
       } else {
         Navigator.pushReplacementNamed(context, login);
@@ -54,7 +43,7 @@ class _LandingState extends State<Landing> {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: CircularProgressIndicator(),  // 加载过程中显示的内容
+        child: CircularProgressIndicator(), // 加载过程中显示的内容
       ),
     );
   }
