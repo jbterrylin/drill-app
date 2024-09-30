6f887edabe52d199671be574e0381e5172389685
秋及北 <wanglinlee1707@gmail.com>
2024-09-30 20:56:23 +0800

    wip MyDrill

--- lib/view/my_drill.dart
+++ lib/view/my_drill.dart
@@ -1,8 +1,8 @@
 import 'package:drill_app/api/api.dart';
-import 'package:drill_app/component/bottom_bar.dart';
 import 'package:drill_app/model/group.dart';
-import 'package:drill_app/util/secure_storage/secure_storage.dart';
+import 'package:drill_app/state/me.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 class MyDrill extends StatefulWidget {
   const MyDrill({super.key});
@@ -27,7 +27,7 @@ class _MyDrillState extends State<MyDrill> {
   Future<void> getGroupListFunc() async {
     GetGroupListReq getGroupListReq = GetGroupListReq(
         baseListReq: BaseListReq(page: 1, pageSize: 10),
-        haveUserId: await getUserId() ?? 0);
+        haveUserId: GetIt.I<MeController>().getMe()?.id ?? 0);
     GetGroupListResp? getGroupListResp =
         await api.groupApi.getGroupList(getGroupListReq);
     if (getGroupListResp?.base?.code == 0) {
