cd22e758bc57b67525a3ca7562216f9cbba5a17f
秋及北 <wanglinlee1707@gmail.com>
2024-10-19 17:40:52 +0800

    update component

--- lib/component/bottom_bar.dart
+++ lib/component/bottom_bar.dart
@@ -1,3 +1,5 @@
+import 'package:animations/animations.dart';
+import 'package:drill_app/constant/design.dart';
 import 'package:drill_app/view/home.dart';
 import 'package:drill_app/view/my_drill.dart';
 import 'package:drill_app/view/personal.dart';
@@ -36,7 +38,7 @@ class _BottomBarState extends State<BottomBar> {
 
   late BottomBarIndex selectedTab;
   late Map<BottomBarIndex, Page> body;
-  
+
   @override
   void initState() {
     super.initState();
@@ -62,8 +64,7 @@ class _BottomBarState extends State<BottomBar> {
     init();
   }
 
-  void init() async {
-  }
+  void init() async {}
 
   void _onItemTapped(int index) {
     selectedTab = BottomBarIndex.values[index];
@@ -76,33 +77,62 @@ class _BottomBarState extends State<BottomBar> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: body[BottomBarIndex.values[selectedTab.index]]!.title,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        leading: const SizedBox(),
+        leadingWidth: 0,
+        centerTitle: false,
+        title: const Text("Drill"),
       ),
-      body: body[BottomBarIndex.values[selectedTab.index]]!.body,
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'Personal',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'Token',
-          ),
-        ],
-        currentIndex: selectedTab.index,
-        selectedItemColor: Theme.of(context).colorScheme.primary,
-        unselectedItemColor: Theme.of(context).colorScheme.inversePrimary,
-        onTap: _onItemTapped,
+      body: PageTransitionSwitcher(
+        duration: defaultDuration,
+        transitionBuilder: (child, animation, secondAnimation) {
+          return FadeThroughTransition(
+            animation: animation,
+            secondaryAnimation: secondAnimation,
+            child: child,
+          );
+        },
+        child: body[BottomBarIndex.values[selectedTab.index]]!.body,
+      ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.only(top: defaultPadding / 2),
+        color: Theme.of(context).brightness == Brightness.light
+            ? Colors.white
+            : const Color(0xFF101015),
+        child: BottomNavigationBar(
+          currentIndex: selectedTab.index,
+          onTap: _onItemTapped,
+          backgroundColor: Theme.of(context).brightness == Brightness.light
+              ? Colors.white
+              : const Color(0xFF101015),
+          type: BottomNavigationBarType.fixed,
+          // selectedLabelStyle: TextStyle(color: primaryColor),
+          selectedFontSize: 12,
+          selectedItemColor: primaryColor,
+          unselectedItemColor: primaryMaterialColor[200],
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              activeIcon: Icon(Icons.home, color: primaryColor),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              activeIcon: Icon(Icons.business, color: primaryColor),
+              label: "Drill",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              activeIcon: Icon(Icons.school, color: primaryColor),
+              label: "Personal",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              activeIcon: Icon(Icons.school, color: primaryColor),
+              label: "Token",
+            ),
+          ],
+        ),
       ),
     );
   }
