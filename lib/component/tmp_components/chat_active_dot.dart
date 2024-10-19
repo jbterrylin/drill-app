f98a714183608bfcf7f8e695f115ec8527a1cad2
秋及北 <wanglinlee1707@gmail.com>
2024-10-19 17:04:01 +0800

    update component add tmp_components

---
+++ lib/component/tmp_components/chat_active_dot.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'package:drill_app/constant/design.dart';
+
+
+class ChatActiveDot extends StatelessWidget {
+  const ChatActiveDot({
+    super.key,
+    this.dotColor = successColor,
+  });
+  final Color dotColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 12,
+      width: 12,
+      decoration: BoxDecoration(
+        color: dotColor,
+        shape: BoxShape.circle,
+        border: Border.all(
+          width: 3,
+          color: Theme.of(context).scaffoldBackgroundColor,
+        ),
+      ),
+    );
+  }
+}
